f142c9fe0d26a8c61b6e79c8c7d324260a60c4cd
dotnetstd <dotnetstd@outlook.com>
2025-08-17 11:10:58 +0800

    [1.0.0.2]
    1.Merge Chart 相关项目;

---
+++ BulletBars/ExampleJsInterop.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace Blazor_Sparkline
+{
+    public class ExampleJsInterop
+    {
+        public static ValueTask<string> Prompt(IJSRuntime jsRuntime, string message)
+        {
+            // Implemented in exampleJsInterop.js
+            return jsRuntime.InvokeAsync<string>(
+                "exampleJsFunctions.showPrompt",
+                message);
+        }
+    }
+}
